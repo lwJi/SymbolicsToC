9e65de13650f7e73d2171630a5db8f1a9bc2f969
Liwei Ji <jiliwei.phys@gmail.com>
2023-05-22 17:21:54 -0400

    add writeio.jl

---
+++ stencils/test.hxx
@@ -0,0 +1,10 @@
+#ifndef TEST_HXX
+#define TEST_HXX
+
+#include <cctk.h>
+namespace WaveToyHigherOrderX {
+
+1//24
+-9//8
+} // namespace WaveToyHigherOrderX
+#endif // #ifndef TEST_HXX
---
+++ writeio.jl
@@ -0,0 +1,21 @@
+module WriteIO
+
+export WriteFile
+
+function WriteFile(headpart::Function, bodypart::Function, tailpart::Function, filename="test.txt")
+  if isfile(filename)
+    println("file ", filename, " already, exist, replacing it ...")
+    rm(filename)
+  end
+  filepointer = open(filename, "a")
+  pr(x="") = println(filepointer, x)
+
+  headpart(pr);
+  bodypart(pr);
+  tailpart(pr);
+
+  println("Done generating ", filename)
+  close(filepointer)
+end
+
+end
