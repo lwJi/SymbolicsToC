33a9a444394ac4e58798216a286c7c57757f5f43
Liwei Ji <jiliwei.phys@gmail.com>
2023-05-24 09:10:25 -0400

    Stencils: update stencils.jl

--- stencils/stencils.jl
+++ stencils/stencils.jl
@@ -2,14 +2,24 @@ module FDStencils
 
 using Symbolics
 
-export GetCoefficient, ReplaceGrids
-
-@variables c(..)
+export GetCoefficient, GetDerivsFD, ReplaceGrids
 
 function GetCoefficient(samples, values)
+  @variables c(..)
   npts = length(samples)
   eqns = [sum([c(i)*samples[i]^(j-1) for i in 1:npts]) ~ values[j] for j in 1:npts]
   return Symbolics.solve_for(eqns, [c(i) for i in 1:npts])
+end
+
+function GetDerivsFD(sample, order=1)
+  @variables gf[1:length(sample)];
+  values = [(i == 2) ? 1 : 0 for i in 1:length(sample)]
+  coeff = GetCoefficient(sample, values)
+  expr = string(sum(coeff[i] * gf[i] for i in 1:length(coeff)))
+  for replacerule in ReplaceGrids(length(sample))
+      expr = replace(expr, replacerule)
+  end
+  expr
 end
 
 function ReplaceGrids(npts)
