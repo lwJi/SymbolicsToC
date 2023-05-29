04b5d1fbc84663dc1348d977861e99cce60f6b90
Liwei Ji <jiliwei.phys@gmail.com>
2023-05-29 17:04:05 -0400

    Add test.jl

--- test/test.hxx
+++ test/test.hxx
@@ -1,6 +1,10 @@
 #ifndef TEST_HXX
 #define TEST_HXX
 
+/* test.hxx */
+/* (c) Liwei Ji 2023-05-29 */
+/* Produced with Julia */
+
 #include <cctk.h>
 namespace WaveToyHigherOrderX {
 
@@ -9,5 +13,7 @@ inline CCTK_ATTRIBUTE_ALWAYS_INLINE CCTK_HOST CCTK_DEVICE T
 calc_fd_c1D2O(const GF3D2<const T> &gf, const PointDesc &p, const int dir) {
   return ((1//12)*gf(p.I - 2*p.DI[dir]) + (2//3)*gf(p.I + p.DI[dir]) - (1//12)*gf(p.I + 2*p.DI[dir]) - (2//3)*gf(p.I - p.DI[dir])) / h;
 }
+
 } // namespace WaveToyHigherOrderX
+
 #endif // #ifndef TEST_HXX
---
+++ test/test.jl
@@ -0,0 +1,55 @@
+# test.jl
+# (c) Liwe Ji, 05/2023
+
+using Dates
+
+include("../writeio.jl")
+using .WriteIO
+include("../codes/carpetx.jl")
+using .CarpetX
+
+#=
+  Wrap functions
+=#
+function PrintDerivsFunction(pr::Function, funcname::String, sample, order=1)
+  pr("template <typename T>")
+  pr("inline CCTK_ATTRIBUTE_ALWAYS_INLINE CCTK_HOST CCTK_DEVICE T")
+  pr(funcname*"(const GF3D2<const T> &gf, const PointDesc &p, const int dir) {")
+  pr("  return "*PrintDerivsFD(sample, order)*";")
+  pr("}")
+end
+
+#=
+  Write to files
+=#
+filename = "test.hxx";
+thornname = "WaveToyHigherOrderX";
+
+function headpart(pr::Function)
+  pr("#ifndef " * replace(uppercase(filename), "."=>"_"))
+  pr("#define " * replace(uppercase(filename), "."=>"_"))
+  pr()
+  pr("/* " * filename * " */")
+  pr("/* (c) Liwei Ji " * string(Dates.today()) * " */")
+  pr("/* Produced with Julia */")
+  pr()
+  pr("#include <cctk.h>")
+  pr("namespace " * thornname * " {")
+  pr()
+end
+
+function bodypart(pr::Function)
+  PrintDerivsFunction(pr, "calc_fd_c1D2O", [-2//1, -1, 0, 1, 2])
+  pr()
+end
+
+function tailpart(pr::Function)
+  pr("} // namespace " * thornname)
+  pr()
+  pr("#endif // #ifndef " * replace(uppercase(filename), "."=>"_"))
+end
+
+#=
+  Main
+=#
+WriteFile(headpart, bodypart, tailpart, filename)
