9b175507ef2e7e88aaf4ad1d22bd771524caf7f1
Liwei Ji <jiliwei.phys@gmail.com>
2023-05-28 23:32:24 -0400

    Stencils: update Stencils.ipynb

--- test/test.hxx
+++ test/test.hxx
@@ -4,7 +4,10 @@
 #include <cctk.h>
 namespace WaveToyHigherOrderX {
 
-1//24
--9//8
+template <typename T>
+inline CCTK_ATTRIBUTE_ALWAYS_INLINE CCTK_HOST CCTK_DEVICE T
+calc_fd_c1D2O(const GF3D2<const T> &gf, const PointDesc &p, const int dir) {
+  return ((1//12)*gf(p.I - 2*p.DI[dir]) + (2//3)*gf(p.I + p.DI[dir]) - (1//12)*gf(p.I + 2*p.DI[dir]) - (2//3)*gf(p.I - p.DI[dir])) / h;
+}
 } // namespace WaveToyHigherOrderX
 #endif // #ifndef TEST_HXX
