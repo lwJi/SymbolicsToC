c25e3aa142f0ef01db86e2f2257a1e9d3abec41e
Liwei Ji <jiliwei.phys@gmail.com>
2023-05-31 10:33:05 -0400

    codes: update carpetx.jl

--- test/test.hxx
+++ test/test.hxx
@@ -12,7 +12,7 @@ namespace WaveToyHigherOrderX {
 template <typename T>
 inline CCTK_ATTRIBUTE_ALWAYS_INLINE CCTK_HOST CCTK_DEVICE T
 calc_fd_c1D2O(const GF3D2<const T> &gf, const PointDesc &p, const int dir) {
-  return ((1/T(12))*gf(p.I - 2*p.DI[dir]) + (2/T(3))*gf(p.I + p.DI[dir]) - (1/T(12))*gf(p.I + 2*p.DI[dir]) - (2/T(3))*gf(p.I - p.DI[dir])) / h;
+  return ((1/T(12))*gf(p.I - 2*p.DI[dir]) + (2/T(3))*gf(p.I + p.DI[dir]) - (1/T(12))*gf(p.I + 2*p.DI[dir]) - (2/T(3))*gf(p.I - p.DI[dir])) / p.DX[dir];
 }
 
 } // namespace WaveToyHigherOrderX
